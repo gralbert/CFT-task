a56c8c912631b35f52553e816d0b670935ed3118
gralbert <albertgralin@gmail.com>
2019-09-16 18:43:54 +0700

    Add class Car.

--- CarClass.py
+++ CarClass.py
@@ -1,9 +1,15 @@
 class Car:
-    def __init__(self):
-        pass
+    def __init__(self, year, producer, model, body_type):
+        self.year = year
+        self.producer = producer
+        self.model = model
+        self.body_type = body_type
 
     def __repr__(self):
         return self.__str__()
 
     def __str__(self):
-        return
+        string = '|{:^10}|{:^14}|{:^12}|{:^12}'\
+            .format(self.year, self.producer,
+                    self.model, self.body_type)
+        return string
