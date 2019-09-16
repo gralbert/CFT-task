3e3a22edd3e36a1964b41287add1c6c9e89af17a
gralbert <albertgralin@gmail.com>
2019-09-16 17:39:16 +0700

    Initial commit.

---
+++ CarClass.py
@@ -0,0 +1,9 @@
+class Car:
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return
---
+++ Main.py
@@ -0,0 +1,40 @@
+"""
+Auto catalogue.
+Developed by Grigorev Albert.
+"""
+
+from CarClass import Car
+
+
+def accept_command():
+    """ Asks for the command number. """
+    # TODO
+
+
+def add(lst):
+    """ Asks user about the car. """
+    # TODO
+
+
+def read():
+    """ Reading from the file. """
+    # TODO
+
+
+def write(lst):
+    """ Writing to the file. """
+    # TODO
+
+
+def show(lst):
+    """ Print objects. """
+    # TODO
+
+
+def main():
+    """ Main function. """
+    # TODO
+
+
+if __name__ == '__main__':
+    main()
