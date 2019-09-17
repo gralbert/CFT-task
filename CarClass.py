455a7bcb4201efaf415ecf880d84a88afee453e8
gralbert <albertgralin@gmail.com>
2019-09-17 22:12:21 +0700

    Final commit.

--- CarClass.py
+++ CarClass.py
@@ -1,15 +1,52 @@
 class Car:
-    def __init__(self, year, producer, model, body_type):
-        self.year = year
-        self.producer = producer
-        self.model = model
-        self.body_type = body_type
+    def __init__(self, data):
+        self._aid = data['_aid']
+        self._year = data['_year']
+        self._producer = data['_producer']
+        self._model = data['_model']
+        self._body_type = data['_body_type']
 
     def __repr__(self):
         return self.__str__()
 
     def __str__(self):
-        string = '|{:^10}|{:^14}|{:^12}|{:^12}'\
-            .format(self.year, self.producer,
-                    self.model, self.body_type)
+        string = '|{:^8}|{:^12}|{:^18}|{:^16}|{:^16}|'\
+            .format(self._aid, self._year, self._producer,
+                    self._model, self._body_type)
         return string
+
+    @property
+    def aid(self):
+        return self._aid
+
+    @property
+    def year(self):
+        return self._year
+
+    @year.setter
+    def year(self, value):
+        self._year = value
+
+    @property
+    def producer(self):
+        return self._producer
+
+    @producer.setter
+    def producer(self, value):
+        self._producer = value
+
+    @property
+    def model(self):
+        return self._model
+
+    @model.setter
+    def model(self, value):
+        self._model = value
+
+    @property
+    def body_type(self):
+        return self._body_type
+
+    @body_type.setter
+    def body_type(self, value):
+        self._body_type = value
--- Main.py
+++ Main.py
@@ -5,15 +5,16 @@ Developed by Grigorev Albert.
 
 from CarClass import Car
 import json
+import random
 
 
 def accept_command():
     """ Asks for the command number. """
     while True:
-        command = input('Введите номер команды: ')
+        command = input('Введите номер команды: ').replace(' ', '').replace('.', '')
         try:
             command = int(command)
-        except TypeError:
+        except ValueError:
             pass
 
         if command in [1, 2, 3, 4, 5]:
@@ -25,23 +26,76 @@ def accept_command():
 def add(lst):
     """ Asks user about the car. """
     data = dict()
-    data['year'] = input('Введите год выпуска автомобиля:')
-    data['model'] = input('Введите модель автомобиля:')
-    data['producer'] = input('Введите производителя:')
-    data['body_type'] = input('Введите тип кузова:')
+    while True:
+        data['_producer'] = input('Введите производителя:')
+        if len(data['_producer']) > 16:
+            print('Длинновато! Постарайтесь сократить.')
+        else:
+            break
 
-    return lst.append(Car(year=data['year'], model=data['model'],
-                          producer=data['producer'], body_type=data['body_type']))
+    while True:
+        data['_model'] = input('Введите модель автомобиля:')
+        if len(data['_producer']) > 14:
+            print('Длинновато! Постарайтесь сократить.')
+        else:
+            break
+
+    while True:
+        data['_year'] = input('Введите год выпуска автомобиля:')
+        if len(data['_producer']) > 4:
+            print('Длинновато! Постарайтесь сократить.')
+        else:
+            break
+
+    while True:
+        data['_body_type'] = input('Введите тип кузова:')
+        if len(data['_producer']) > 14:
+            print('Длинновато! Постарайтесь сократить.')
+        else:
+            break
+
+    # Generate auto's id.
+    set_of_ids = set()
+    for obj in lst:
+        set_of_ids.add(obj.aid)
+
+    data['_aid'] = random.randint(1000, 9999)
+    while data['_aid'] in set_of_ids:
+        data['_aid'] = random.randint(1000, 9999)
+
+    return lst.append(Car(data))
+
+
+def delete(lst):
+    """ Delete car from the list. """
+    try:
+        aid = int(input('Введите ID автомобиля для удаления: '))
+    except ValueError:
+        print('\nID указан неверно.\n')
+        return lst
+
+    for num in range(len(lst)):
+        if lst[num].aid == aid:
+            lst.pop(num)
+            print('Успешно удалено.\n')
+            return lst
+    print('\nID указан неверно. Повторите попытку. \n')
+    return lst
 
 
 def read():
     """ Reading from the file. """
     lst = []
-    with open('data.dot') as f:
-        for line in f.readlines():
-            data = json.loads(line)
-            lst.append(Car(year=data['year'], model=data['model'],
-                           producer=data['producer'], body_type=data['body_type']))
+    try:
+        with open('data.dot') as f:
+            for line in f.readlines():
+                data = json.loads(line)
+                lst.append(Car(data))
+
+    except FileNotFoundError:
+        print('Внимание! Файл с данными не найден. Создан новый. \n')
+        with open('data.dot', 'w') as f:
+            print('', file=f, end='')
 
     return lst
 
@@ -55,16 +109,81 @@ def write(lst):
 
 def show(lst):
     """ Print objects. """
-    header = '|{:^10}|{:^14}|{:^12}|{:^12}'.format('Год', 'Производитель',
-                                                   'Модель', 'Тип кузова')
+    header = '|{:^8}|{:^12}|{:^18}|{:^16}|{:^16}|'.format('ID', 'Год', 'Производитель',
+                                                          'Модель', 'Тип кузова')
     print(header)
     for elem in lst:
         print(elem)
 
 
+def edit(lst):
+    """ Edit information about car. """
+    try:
+        aid = int(input('Введите ID автомобиля для редактирования: '))
+    except ValueError:
+        print('\nID указан неверно.\n')
+        return lst
+
+    print('Что редактируем? \n'
+          '1. Производитель \n'
+          '2. Модель \n'
+          '3. Год выпуска \n'
+          '4. Тип кузова \n')
+
+    try:
+        cmd = int(input('Введите номер: ').replace(' ', '').replace('.', ''))
+    except ValueError:
+        print('\nНомер указан неверно.\n')
+        return lst
+
+    new_value = input('Введите новое значение: ')
+
+    for num in range(len(lst)):
+        if lst[num].aid == aid:
+            if cmd == 1:
+                lst[num].producer = new_value
+                return lst
+            elif cmd == 2:
+                lst[num].model = new_value
+                return lst
+            elif cmd == 3:
+                lst[num].year = new_value
+                return lst
+            elif cmd == 4:
+                lst[num].body_type = new_value
+                return lst
+            print('\nНомер указан неверно. Повторите попытку. \n')
+            return lst
+    print('\nID указан неверно. Повторите попытку. \n')
+    return lst
+
+
 def main():
     """ Main function. """
-    # TODO
+    lst = read()
+    while True:
+        print('1. Просмотреть информацию об автомобилях. \n'
+              '2. Добавить новый автомобиль. \n'
+              '3. Удалить автомобиль из списка. \n'
+              '4. Редактировать информацию об автомобиле. \n'
+              '5. Выход.')
+        command = accept_command()
+
+        if command == 1:
+            show(lst)
+        elif command == 2:
+            add(lst)
+            write(lst)
+        elif command == 3:
+            lst = delete(lst)
+            write(lst)
+        elif command == 4:
+            edit(lst)
+            write(lst)
+        else:
+            break
+
+    print('Спасибо за визит!')
 
 
 if __name__ == '__main__':
